2d658cf9ad7abd902cf79f2c9f9ed3af912764fa
Amrit Pant <info.amritpant@gmail.com>
2022-06-18 13:05:20 +0545

    Added Base Server Side Rendering Routers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const router = require('./Routes/router');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/', router);
 
 module.exports = app;
