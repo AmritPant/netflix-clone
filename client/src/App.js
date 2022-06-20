98f4827e851c501f9326308d5779d3ec0d3b7012
Amrit Pant <info.amritpant@gmail.com>
2022-06-20 13:00:58 +0545

    Added Basic React Router Dom setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import LoggedOutHome from './pages/LoggedOutHome/LoggedOutHome';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <LoggedOutHome />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoggedOutHome />} />
+      </Routes>
+    </Router>
   );
 }
 
