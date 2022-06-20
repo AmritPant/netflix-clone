e17fe26cc3a6a45f5b9edf96a1ba1676e31f9544
Amrit Pant <info.amritpant@gmail.com>
2022-06-20 13:15:37 +0545

    Added Flexibility to remove Link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import NetflixLogo from '../../assets/images/netflix-logo-logout.svg';
 
-function Header() {
+function Header(props) {
   return (
     <header className="loggedOutHome__header">
       <img src={NetflixLogo} alt="Netflix Logo Red Color" />
-      <a href="/login">Sign In</a>
+      {props.showLoginButton !== 'false' && <a href="/login">Sign In</a>}
     </header>
   );
 }
