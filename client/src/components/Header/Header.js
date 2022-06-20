00fc40e8c84c4813b0929594528c32350c6f9cc3
Amrit Pant <info.amritpant@gmail.com>
2022-06-20 14:04:44 +0545

    Make Header logo as a link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,9 @@ import NetflixLogo from '../../assets/images/netflix-logo-logout.svg';
 function Header(props) {
   return (
     <header className="loggedOutHome__header">
-      <img src={NetflixLogo} alt="Netflix Logo Red Color" />
+      <a href="/">
+        <img src={NetflixLogo} alt="Netflix Logo Red Color" />
+      </a>
       {props.showLoginButton !== 'false' && (
         <a href="/login" className="button-primary">
           Sign In
